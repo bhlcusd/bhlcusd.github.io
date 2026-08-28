471a300924069a97f4dc0cedcd136245d4845e60
bhlcusd <242719234+bhlcusd@users.noreply.github.com>
2026-08-28 07:54:13 -0700

    daily 8-28-26

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+
+</script>
+
+<svelte:head>
+	<title>Brandon's Site</title>
+</svelte:head>
+
+<header>
+
+</header>
+<main>
+
+</main>
+<footer>
+	
+</footer>
